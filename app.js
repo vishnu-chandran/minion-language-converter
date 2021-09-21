00ddfe787af1dcf2489369756adcc7610e1f6772
vishnu <vishnuchandran19194@gmail.com>
2021-09-21 19:55:01 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var btntranslate = document.querySelector("#btn-translate");
+var textinput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output")
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = textinput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
+};
+
+btntranslate.addEventListener("click", clickHandler)
